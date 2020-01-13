357df7f50c63787f61db2f7e72c5a644595b4f1b
Jonatas <modesto.jonatas18@gmail.com>
2020-01-13 20:02:12 -0300

    Projeto que calcula pagamento de funcionarios

---
+++ Polimorfismo e Herança exercício 1/Entities/OutsourcedEmployee.cs
@@ -0,0 +1,22 @@
+﻿namespace Polimorfismo_e_Herança_exercício_1.Entities
+{
+    class OutsourcedEmployee : Employee
+    {
+        public double AdditionalCharge { get; set; }
+
+        public OutsourcedEmployee()
+        {
+        }
+
+        public OutsourcedEmployee(string name, int hours, double valuePerHour, double additionalCharge) : base (name, hours, valuePerHour)
+        {
+            AdditionalCharge = additionalCharge;
+        }
+
+        public override double Payment()
+        {
+            return base.Payment() + AdditionalCharge * 1.1;
+            
+        }
+    }
+}
